dfae82904c20679ca3496135122cda96274b880b
Valdecir Silva <valdecir.silva@cp2.edu.br>
2017-05-22 17:21:35 -0300

    Correção de erros de portugês, padronização de posicionamento de métpdos e melhoria na identação.

--- src/dominio/ListaLinearSequencial.java
+++ src/dominio/ListaLinearSequencial.java
@@ -1,13 +1,16 @@
 package dominio;
 
-import java.awt.font.LayoutPath;
-
 public class ListaLinearSequencial {
 	private int[] lista;
-	private int ultimaPos; /* INDICE DA ULTIMA POSIÇÃO PEENCHIDA */
+	private int ultimaPos; // ÍNDICE DA ÚLTIMA POSIÇÃO PREENCHIDA
 
+	public ListaLinearSequencial(int tamanho) {
+		this.lista	= new int[tamanho];
+		ultimaPos	= -1; // LISTA VAZIA
+	}
+	
 	public int[] getLista() {
-		return lista;
+		return this.lista;
 	}
 
 	public void setLista(int[] lista) {
@@ -15,69 +18,65 @@ public class ListaLinearSequencial {
 	}
 
 	public int getUltimaPos() {
-		return ultimaPos;
+		return this.ultimaPos;
 	}
 
 	public void setUltimaPos(int ultimaPos) {
 		this.ultimaPos = ultimaPos;
 	}
 
-	public ListaLinearSequencial(int tamanho) {
-		this.lista = new int[tamanho];
-		ultimaPos = -1; /* LISTA VAZIA */
-	}
-
-	/* MÉTODO PARA VERIFICAR SE A LISTA ESTÁ CHEIA */
+	// MÉTODO PARA VERIFICAR SE A LISTA ESTÁ CHEIA
 	public boolean listaCheia() {
-		if (ultimaPos == lista.length - 1)
+		if (this.ultimaPos == lista.length - 1)
 			return true;
 		else
 			return false;
 	}
 
-	/* MÉTODO PARA VERIFICR SE ESTÁ VAZIO */
+	// MÉTODO PARA VERIFICAR SE A LISTA ESTÁ VAZIA
 	public boolean listaVazia() {
-		if (ultimaPos == -1)
+		if (this.ultimaPos == -1)
 			return true;
 		else
 			return false;
 	}
 
-	/* INSERE "ELEMENTO" NA LISTA LINEAR SEQUENCIAL SE TIVER ESPAÇO */
+	// INSERE "ELEMENTO" NA LISTA LINEAR SEQUENCIAL SE TIVER ESPAÇO
 	public boolean inserirElemento(int elemento) {
-		if (listaCheia()) {
+		if (listaCheia()) 
 			return false;
-		} else {
+		else {
 			this.lista[++ultimaPos] = elemento;
+			
 			return true;
 		}
 	}
 
-	/* REMOVE A PRIMEIRA OCORRENCIA DO ELEMENTO */
+	// REMOVE A PRIMEIRA OCORRÊNCIA DO ELEMENTO
 	public boolean removeElemento(int elemento) {
-		if (listaVazia()) {
+		if (listaVazia())
 			return false;
-		} else {
+		else {
 			int achou = this.buscaElemento(elemento);
-			if (achou >= 0) {
+			
+			if (achou >= 0)
 				this.lista[achou] = this.lista[ultimaPos--];
-			}
+			
 			return true;
 		}
 	}
 
-	/* BUSCAR DE FORMA SEQUÊNCIAL */
+	// BUSCAR DE FORMA SEQUENCIAL
 	public int buscaElemento(int elemento) {
-		if (listaVazia()) {
+		if (listaVazia())
 			return -1;
-		} else {
+		else {
 			for (int indice = 0; indice < ultimaPos + 1; indice++) {
-				if (elemento == this.lista[indice]) {
+				if (elemento == this.lista[indice])
 					return indice;
-				}
 			}
-			return -2; /*-2 QUANDO SE PROCURA E NÃO ACHA*/ 
+			
+			return -2; // -2 QUANDO SE PROCURA E NÃO ACHA 
 		}
 	}
-
 }
