b202c1a13981b4999b80cf91c69393fabaaa77b3
Valdecir Silva <valdecir.silva@cp2.edu.br>
2017-05-22 16:49:45 -0300

    teste

--- src/dominio/ListaLinearSequencial.java
+++ src/dominio/ListaLinearSequencial.java
@@ -76,7 +76,7 @@ public class ListaLinearSequencial {
 					return indice;
 				}
 			}
-			return -2; /*-2 QUANDO SE PROCURA E NÃO ACHA*/
+			return -2; /*-2 QUANDO SE PROCURA E NÃO ACHA*/ //teste
 		}
 	}
 
